688cc52efdd8ff2eba14a6e19cd2285cc5bfcef3
Paul <paul@Paulo>
2018-07-26 15:16:54 +0000

    Add views and urls

--- src/profiles_api/views.py
+++ src/profiles_api/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
-
+from rest_framework.views import APIView
+from rest_framework.response import Response
 # Create your views here.
+
+class HelloAPIView(APIView):
+    """This class model APIView"""
+
+    def get(self, request, format=None):
+        """Returns a list of computer programming languages."""
+        the_list = [
+            'python programming',
+            'JAva',
+            'perl',
+            'php',
+            'GO',
+            'JS',
+            'Swift'
+        ]
+
+        return Response({"These are the programming languages i have":the_list})
